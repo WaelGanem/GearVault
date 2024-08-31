2c0f44efa97e019cafb7ff18652711b12830336c
znxaz <zein.azzam@outlook.com>
2024-08-31 17:23:10 +0300

    added userSlice

--- src/store/Slices/userSlice.ts
+++ src/store/Slices/userSlice.ts
@@ -1,5 +1,27 @@
-import {PayloadAction, createSlice} from "@reduxjs/toolkit"
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { UserData } from "../../types/UserData";
 
-export interface userState {
-    user: userData
+export interface UserState {
+  user: UserData | null;
 }
+
+const initialState: UserState = {
+    user: null,
+}; 
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+      setUser: (state, action: PayloadAction<Partial<UserData>>) => {
+        if (state.user) {
+          state.user = { ...state.user, ...action.payload };
+        } else {
+          state.user = action.payload as UserData;
+        }
+      },
+      clearUser: (state) => {
+        state.user = null;
+      },
+    },
+  });
--- src/types/UserData.ts
+++ src/types/UserData.ts
@@ -4,4 +4,5 @@ export interface UserData {
     lastName: string;
     email: string; 
     phoneNumber: string; 
+    isLoggedIn: boolean; 
 }
